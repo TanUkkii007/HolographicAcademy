5c09bf3d6732f702d5b20602116f67fb5edaf4d5
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-03-04 17:16:47 +0900

    Chapter 3 - Gestures

---
+++ Origami/Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GazeGestureManager : MonoBehaviour {
+
+    public static GazeGestureManager Instance { get; private set; }
+
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Instance = this;
+
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) => {
+            if (FocusedObject != null)
+            {
+                FocusedObject.SendMessageUpwards("OnSelect");
+            }
+        };
+        recognizer.StartCapturingGestures();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject oldFocusObject = FocusedObject;
+
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            FocusedObject = hitInfo.collider.gameObject;
+        }
+        else
+        {
+            FocusedObject = null;
+        }
+
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+    }
+}
---
+++ Origami/Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCommands : MonoBehaviour {
+
+    void OnSelect()
+    {
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
