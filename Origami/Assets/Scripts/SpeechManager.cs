af0f210436f58ea9fa4cbb0a99712ee6aa18eb63
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-03-04 21:46:53 +0900

    Chapter 4 - Voice

---
+++ Origami/Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SpeechManager : MonoBehaviour {
+
+    UnityEngine.Windows.Speech.KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start() {
+        keywords.Add("Reset world", () => {
+            this.BroadcastMessage("OnReset");
+        });
+
+        keywords.Add("Drop Sphere", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("OnDrop");
+            }
+        });
+
+        keywordRecognizer = new UnityEngine.Windows.Speech.KeywordRecognizer(keywords.Keys.ToArray());
+
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(UnityEngine.Windows.Speech.PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Origami/Assets/Scripts/SphereCommands.cs
+++ Origami/Assets/Scripts/SphereCommands.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SphereCommands : MonoBehaviour {
 
+    Vector3 originalPosition;
+
     void OnSelect()
     {
         if (!this.GetComponent<Rigidbody>())
@@ -13,11 +15,27 @@ public class SphereCommands : MonoBehaviour {
         }
     }
 
+    void OnReset()
+    {
+        var rigidbody = this.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            DestroyImmediate(rigidbody);
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+        this.transform.localPosition = originalPosition;
+    }
+
+    void OnDrop()
+    {
+        OnSelect();
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        originalPosition = this.transform.localPosition;
+    }
 	
 	// Update is called once per frame
 	void Update () {
